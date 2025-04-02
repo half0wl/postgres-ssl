688e23f270698556d43982971d2d61b3371cc92b
Ray Chen <ray@raychen.io>
2025-04-03 04:47:35 +0800

    .

--- beta/start.sh
+++ beta/start.sh
@@ -5,6 +5,7 @@ source "/usr/local/bin/_include.sh"
 ENSURE_SSL_SCRIPT="/usr/local/bin/ensure-ssl.sh"
 CONFIGURE_PRIMARY_SCRIPT="/usr/local/bin/configure-primary.sh"
 CONFIGURE_REPLICA_SCRIPT="/usr/local/bin/configure-replica.sh"
+PG_CONF_FILE="${PGDATA}/postgresql.conf"
 
 # Check the RAILWAY_PG_INSTANCE_MODE environment variable
 if [ -n "$RAILWAY_PG_INSTANCE_MODE" ]; then
