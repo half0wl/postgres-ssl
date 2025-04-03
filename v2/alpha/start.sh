e9ff7ee2cdea7a60812e11a34f6d1ac0328e1ba4
Ray Chen <ray@raychen.io>
2025-04-04 01:27:04 +0800

    .

--- v2/alpha/start.sh
+++ v2/alpha/start.sh
@@ -39,26 +39,46 @@ log_err() {
   echo -e "[ ${RED_R}⛔ ERR${NC}  ] ${RED_B}$1${NC}" >&2
 }
 
-log_warn "THIS IS AN ALPHA RELEASE."
-log_warn "Do not use this version unless advised by Railway staff."
+echo ""
+echo ""
+echo "--------------------------------------------------------------------"
+log_warn "This is an ALPHA version of the Railway Postgres image."
+log_warn ""
+log_warn "DO NOT USE THIS VERSION UNLESS ADVISED BY RAILWAY STAFF."
+log_warn ""
+log_warn "This version must only be used with direct support from"
+log_warn "Railway. If we did not ask you to use this version,"
+log_warn "please do not."
+log_warn ""
+log_warn "If you choose to use this version WITHOUT BEING ADVISED"
+log_warn "OR ASKED TO by the Railway team:"
+log_warn ""
+log_warn "  You accept that you are doing so at your own risk,"
+log_warn "  and Railway is not responsible for any data loss"
+log_warn "  or corruption that may occur as a result of"
+log_warn "  ignoring this warning."
+echo "--------------------------------------------------------------------"
+echo ""
+echo ""
 
 if [ ! -z "$DEBUG_MODE" ]; then
   log "Starting in debug mode! Postgres will not run."
   log "The container will stay alive and be shell-accessible."
   trap "echo Shutting down; exit 0" SIGTERM SIGINT SIGKILL
-  sleep infinity & wait
+  sleep infinity &
+  wait
 fi
 
 # Ensure required environment variables are set
-REQUIRED_ENV_VARS=(\
-    "RAILWAY_VOLUME_NAME" \
-    "RAILWAY_VOLUME_MOUNT_PATH" \
+REQUIRED_ENV_VARS=(
+  "RAILWAY_VOLUME_NAME"
+  "RAILWAY_VOLUME_MOUNT_PATH"
 )
 for var in "${REQUIRED_ENV_VARS[@]}"; do
-    if [ -z "${!var}" ]; then
-        log_err "Missing required environment variable: $var"
-        exit 1
-    fi
+  if [ -z "${!var}" ]; then
+    log_err "Missing required environment variable: $var"
+    exit 1
+  fi
 done
 
 # PGDATA dir
@@ -86,27 +106,26 @@ READ_REPLICA_MUTEX="${REPMGR_DIR}/rrmutex"
 # Run different setup according to RAILWAY_PG_INSTANCE_TYPE
 if [ -n "$RAILWAY_PG_INSTANCE_TYPE" ]; then
   case "$RAILWAY_PG_INSTANCE_TYPE" in
-    "READREPLICA")
-      # Configure as read replica
-      if [ -f "$READ_REPLICA_MUTEX" ]; then
-        log "Read replica appears to be configured. Skipping."
-      else
-        log_hl "Configuring as read replica"
-        source "$SH_READREPLICA"
-      fi
-      ;;
-    "PRIMARY")
-      # Configure as primary
-      if grep -q \
-        "include 'postgresql.replication.conf'" "$PG_CONF_FILE" 2>/dev/null;
-          then
-            log "Replication appears to be configured. Skipping."
-      else
-          log_hl "Configuring as primary"
-          source "$SH_PRIMARY"
-      fi
-      ;;
-    *)
+  "READREPLICA")
+    # Configure as read replica
+    if [ -f "$READ_REPLICA_MUTEX" ]; then
+      log "Read replica appears to be configured. Skipping."
+    else
+      log_hl "Configuring as read replica"
+      source "$SH_READREPLICA"
+    fi
+    ;;
+  "PRIMARY")
+    # Configure as primary
+    if grep -q \
+      "include 'postgresql.replication.conf'" "$PG_CONF_FILE" 2>/dev/null; then
+      log "Replication appears to be configured. Skipping."
+    else
+      log_hl "Configuring as primary"
+      source "$SH_PRIMARY"
+    fi
+    ;;
+  *) ;;
   esac
 fi
 
