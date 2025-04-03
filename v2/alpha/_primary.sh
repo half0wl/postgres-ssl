de065da1fafaf3a9851484f2a94c9a97d8620bdb
Ray Chen <ray@raychen.io>
2025-04-04 01:10:36 +0800

    .

--- v2/alpha/_primary.sh
+++ v2/alpha/_primary.sh
@@ -14,7 +14,7 @@ if [ -z "$REPMGR_USER_PWD" ]; then
   exit 1
 fi
 
-wait_for_postgres() {
+wait_for_postgres_start() {
   local sleep_time=3
   local max_attempts=10
   local attempt=1
@@ -22,7 +22,8 @@ wait_for_postgres() {
   log "Waiting for Postgres to start..."
 
   while [ $attempt -le $max_attempts ]; do
-    log "Attempt $attempt/$max_attempts: Postgres not ready yet, waiting $sleep_time seconds..."
+    log "Attempt $attempt/$max_attempts: Postgres not ready yet"
+    log "Waiting $sleep_time seconds..."
     if psql $connection_string -c "SELECT 1;" >/dev/null 2>&1; then
       log_ok "Postgres is up and running!"
       return 0
@@ -31,7 +32,8 @@ wait_for_postgres() {
     attempt=$((attempt + 1))
   done
 
-  log_err "Timed out waiting for Postgres to start after $((max_attempts * sleep_time)) seconds."
+  log_err "Timed out waiting for Postgres to start"
+  log_err "(exceeded $((max_attempts * sleep_time)) seconds)"
   return 1
 }
 
@@ -43,41 +45,40 @@ else
   su -m postgres -c "pg_ctl -D ${PGDATA} start"
 
   # Wait for Postgres to be ready after starting
-  wait_for_postgres || {
+  wait_for_postgres_start || {
     log_err "Failed to start Postgres properly. Exiting."
     exit 1
   }
 fi
 
 PG_REPLICATION_CONF_FILENAME="postgresql.replication.conf"
-PG_REPLICATION_CONF_FILE="${PG_DATA_DIR}/${PG_REPLICATION_CONF_FILENAME}"
+PG_REPLICATION_CONF_FILE="${PGDATA}/${PG_REPLICATION_CONF_FILENAME}"
 PG_HBA_CONF_FILE="${PGDATA}/pg_hba.conf"
 
 # Create repmgr user and database
 log "Creating repmgr user and database ⏳"
 if ! psql -c "SELECT 1 FROM pg_roles WHERE rolname='repmgr'" | grep -q 1; then
   psql -c "CREATE USER repmgr WITH SUPERUSER PASSWORD '${REPMGR_USER_PWD}';"
-  log_ok "Created repmgr user"
+  log_ok "repmgr user created with password '***${REPMGR_USER_PWD: -4}'"
 else
-  log "User repmgr already exists"
+  log "repmgr user already exists"
 fi
 
 if ! psql -c "SELECT 1 FROM pg_database WHERE datname='repmgr'" | grep -q 1; then
   psql -c "CREATE DATABASE repmgr;"
   log_ok "Created repmgr database"
 else
-  log "Database repmgr already exists"
+  log "repmgr database already exists"
 fi
 
 # Grant permissions to repmgr user
 psql -c "GRANT ALL PRIVILEGES ON DATABASE repmgr TO repmgr;"
 psql -c "ALTER USER repmgr SET search_path TO repmgr, railway, public;"
 
-log_ok "Configured repmgr user and database permissions"
+log_ok "repmgr database bootstrapped"
 
 # Create repmgr configuration file
 # (node_id is always 1 on primary)
-log "Creating repmgr configuration file at '$REPMGR_CONF_FILE'"
 cat >"$REPMGR_CONF_FILE" <<EOF
 node_id=1
 node_name='node1'
@@ -88,10 +89,9 @@ monitoring_history=yes
 EOF
 sudo chown postgres:postgres "$REPMGR_CONF_FILE"
 sudo chmod 700 "$REPMGR_CONF_FILE"
-log "Created repmgr configuration at '$REPMGR_CONF_FILE'"
+log_ok "Created repmgr config ->> '$REPMGR_CONF_FILE'"
 
 # Create replication configuration file
-log "Creating replication configuration file at '$PG_REPLICATION_CONF_FILE'"
 cat >"$PG_REPLICATION_CONF_FILE" <<EOF
 max_wal_senders = 20
 max_replication_slots = 20
@@ -104,17 +104,17 @@ archive_command = '/bin/true'
 EOF
 sudo chown postgres:postgres "$PG_REPLICATION_CONF_FILE"
 sudo chmod 700 "$PG_REPLICATION_CONF_FILE"
-log "Created replication configuration at '$PG_REPLICATION_CONF_FILE'"
+log_ok "Created Postgres replication config ->> '$PG_REPLICATION_CONF_FILE'"
 
 # Modify PG_CONF_FILE to include replication conf
 PG_CONF_FILE_BAK="${PG_CONF_FILE}.$(date +'%d-%m-%Y_%H-%M-%S').bak"
-log "Backing up '$PG_CONF_FILE' to '$PG_CONF_FILE_BAK' ⏳"
 cp $PG_CONF_FILE $PG_CONF_FILE_BAK
-log "'$PG_CONF_FILE' backed up to '$PG_CONF_FILE_BAK'"
+log "Backed up '$PG_CONF_FILE' ->> '$PG_CONF_FILE_BAK'"
 
 if ! grep -q "include '$PG_REPLICATION_CONF_FILENAME'" "$PG_CONF_FILE"; then
   echo "" >>"$PG_CONF_FILE"
-  echo "# Added by Railway on $(date +'%Y-%m-%d %H:%M:%S') [_primary.sh]" >>"$PG_CONF_FILE"
+  echo "# Added by Railway on $(date +'%Y-%m-%d %H:%M:%S') [_primary.sh]" \
+    >>"$PG_CONF_FILE"
   echo "include '$PG_REPLICATION_CONF_FILENAME'" >>"$PG_CONF_FILE"
   log_ok "Added include directive for replication conf to '$PG_CONF_FILE'"
 else
@@ -135,16 +135,16 @@ if su -m postgres -c "repmgr -f $REPMGR_CONF_FILE primary register"; then
     log_err "Skipping pg_hba.conf modification"
   else
     PG_HBA_CONF_FILE_BAK="${PG_HBA_CONF_FILE}.$(date +'%d-%m-%Y_%H-%M-%S').bak"
-    log "Backing up '$PG_HBA_CONF_FILE' to '$PG_HBA_CONF_FILE_BAK' ⏳"
     cp $PG_HBA_CONF_FILE $PG_HBA_CONF_FILE_BAK
-    log "'$PG_HBA_CONF_FILE' backed up to '$PG_HBA_CONF_FILE_BAK'"
+    log "Backed up '$PG_HBA_CONF_FILE' ->> '$PG_HBA_CONF_FILE_BAK'"
 
     # Create temporary file with the desired content
     _TMPFILE=$(mktemp)
     # Get all lines except the last one
     head -n -1 "$PG_HBA_CONF_FILE" >"$_TMPFILE"
     # Add our new line
-    echo "# Added by Railway on $(date +'%Y-%m-%d %H:%M:%S') [_primary.sh]" >>"$_TMPFILE"
+    echo "# Added by Railway on $(date +'%Y-%m-%d %H:%M:%S') [_primary.sh]" \
+      >>"$_TMPFILE"
     echo "host replication repmgr ::0/0 trust" >>"$_TMPFILE"
     # Add the last line back
     echo "host all all all scram-sha-256" >>"$_TMPFILE"
@@ -156,12 +156,11 @@ if su -m postgres -c "repmgr -f $REPMGR_CONF_FILE primary register"; then
 
     log_ok "Updated '$PG_HBA_CONF_FILE' with replication access."
   fi
-  log_ok "Primary node configuration complete."
+  log_ok "Primary configuration complete."
 else
   log_err "Failed to register primary node with repmgr."
 fi
 
-log_ok "Primary configuration completed."
 log_hl "Stopping Postgres ⏳"
 su -m postgres -c "pg_ctl -D ${PGDATA} stop -m fast"
 
@@ -178,5 +177,6 @@ wait_for_postgres_stop || {
 if pg_ctl -D "$PGDATA" status >/dev/null 2>&1; then
   log_warn "Postgres is still running despite stop attempts."
 else
-  log_ok "Postgres has stopped successfully. The entrypoint will restart it with the new configuration."
+  log_ok "Postgres has stopped successfully. The entrypoint will restart it"
+  log_ok "with the new configuration."
 fi
